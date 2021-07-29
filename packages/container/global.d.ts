1b2383441e724475cd349c31ee5bc94336098918
xx <xxgjzftd@gmail.com>
2021-07-29 17:26:10 +0800

    ~

--- packages/container/global.d.ts
+++ packages/container/global.d.ts
@@ -3,3 +3,9 @@ declare module 'routes' {
   const routes: RouteRecordRaw[]
   export default routes
 }
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}

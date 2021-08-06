2a7a651fa6231175b224b2d5659c9972a98be53a
xx <xxgjzftd@gmail.com>
2021-08-06 14:26:26 +0800

    ~

--- packages/container/src/app.vue
+++ packages/container/src/app.vue
@@ -1,11 +1,14 @@
 <template>
   <h1>app</h1>
   <button @click="goToSupplier">go to supplier</button>
+  <phone-filled></phone-filled>
+  <message-outlined></message-outlined>
   <router-view></router-view>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
+import { PhoneFilled, MessageOutlined } from '@ant-design/icons-vue'
 
 const router = useRouter()
 const goToSupplier = () => {

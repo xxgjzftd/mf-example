b08cfb73805ef1e5ece8242d975b6cea5f0fb740
xx <xxgjzftd@gmail.com>
2021-09-14 14:08:46 +0800

    ~

--- packages/v2/container/src/pages/layout.vue
+++ packages/v2/container/src/pages/layout.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>v2 layout</h1>
-  <el-button>xx</el-button>
-  <router-view></router-view>
+  <div>
+    <h1>v2 layout</h1>
+    <el-button>xx</el-button>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
